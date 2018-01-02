e4985132d5c30d9dc6ce670c0818dbd1d2389b26
Megawats777 <jedsemilla@gmail.com>
2018-01-01 19:28:22 -0500

    Added variables and functions to the MainMenu class

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,8 +6,26 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
 	// Global ui elements
+	[HeaderAttribute("Global UI Elements")]
 	public Text currentScreenIndicator;
 	public Text loadingText;
+
+
+	// HUD layers
+	[HeaderAttribute("HUD Layers")]
+	public GameObject welcomeScreen;
+	public GameObject helpScreen;
+	public GameObject optionsScreen;
+	public GameObject creditsScreen;
+
+
+	// Welcome screen elements
+	[HeaderAttribute("Welcome Screen Elements")]
+	public Button playButton;
+	public Button helpButton;
+	public Button optionsButton;
+	public Button creditsButton;
+	public Button quitButton;
 
     // Use this for initialization
     void Start()
@@ -20,4 +38,51 @@ public class MainMenu : MonoBehaviour
     {
 
     }
+
+	// UI visibiliy functions
+
+	void welcomeScreen_show()
+	{
+
+	}
+
+	void welcomeScreen_hide()
+	{
+
+	}
+
+
+
+	void helpScreen_show()
+	{
+
+	}
+
+	void helpScreen_hide()
+	{
+
+	}
+
+
+
+	void optionsScreen_show()
+	{
+
+	}
+
+	void optionsScreen_hide()
+	{
+
+	}
+
+
+	void creditsScreen_show()
+	{
+
+	}
+
+	void creditsScreen_hide()
+	{
+		
+	}
 }
