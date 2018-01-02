d6be6bb6b9a8dd74aeda8c2cd783297b82fe2f59
Megawats777 <jedsemilla@gmail.com>
2018-01-01 19:20:26 -0500

    Added main menu class

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+	// Global ui elements
+	public Text currentScreenIndicator;
+	public Text loadingText;
+
+    // Use this for initialization
+    void Start()
+    {
+		loadingText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
