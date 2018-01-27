56ec443516b37c28d15f99db612d5c3a1147df21
Megawats777 <jedsemilla@gmail.com>
2018-01-27 18:25:47 -0500

    Added prop set manager and started work on the second prop set

---
+++ Assets/Scripts/PropSetManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropSetManager : MonoBehaviour
+{
+    // List of prop sets
+    [SerializeField]
+    private GameObject[] propSets;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // Disable all prop sets
+        foreach (GameObject currentSet in propSets)
+        {
+            if (currentSet)
+            {
+                currentSet.SetActive(false);
+            }
+        }
+
+        // Select a random prop set to enable
+        int selectionNum = Random.Range(0, propSets.Length);
+
+        if (propSets[selectionNum])
+        {
+            propSets[selectionNum].SetActive(true);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
