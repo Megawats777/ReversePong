d208061485546f488e9b3dbfc7ec8c49d64d0556
Megawats777 <jedsemilla@gmail.com>
2018-02-26 18:39:24 -0500

    Added functionality to the play and quit buttons on the new welcome screen

--- Assets/Scripts/MainMenuScripts/MainMenu_LayerController.cs
+++ Assets/Scripts/MainMenuScripts/MainMenu_LayerController.cs
@@ -1,16 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class MainMenu_LayerController : MonoBehaviour {
+public class MainMenu_LayerController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
+	// Layer references
+	[Header("Layer References"), SerializeField]
+	private MainMenu_WelcomeLayer welcomeScreen;
+
+
+    // Use this for initialization
+    void Start()
+    {
+		welcomeScreen.gameObject.SetActive(true);
+		show_welcomeScreen();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+	/*--Show and Hide layer methods--*/
+
+	public void show_welcomeScreen()
+	{
+		welcomeScreen.show();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void hide_welcomeScreen()
+	{
+		welcomeScreen.hide();
 	}
+
+	/*--Show and Hide layer methods end--*/
 }
--- Assets/Scripts/MainMenuScripts/MainMenu_WelcomeLayer.cs
+++ Assets/Scripts/MainMenuScripts/MainMenu_WelcomeLayer.cs
@@ -1,16 +1,91 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class MainMenu_WelcomeLayer : MonoBehaviour {
+public class MainMenu_WelcomeLayer : MonoBehaviour
+{
+	// Element references
+	[Header("Element References"), SerializeField]
+	private GameObject contentRoot;
+	[SerializeField]
+	private Text loadingText;
+	[SerializeField]
+	private Button playButton;
+	[SerializeField]
+	private Button helpButton;
+	[SerializeField]
+	private Button creditsButton;
+	[SerializeField]
+	private Button quitButton;
+	[SerializeField]
+	private Animator fadeImage;
 
-	// Use this for initialization
-	void Start () {
-		
+	// External references
+	private MainMenu_LayerController mainMenu_LayerController;
+
+	// Called before start
+	void Awake()
+	{
+		mainMenu_LayerController = FindObjectOfType<MainMenu_LayerController>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+	// Show this layer
+	public void show()
+	{
+		// Assign functions to buttons
+		playButton.onClick.AddListener(delegate{
+			StartCoroutine(startGame());
+		});
+
+
+		quitButton.onClick.AddListener(delegate{
+			Application.Quit();
+		});
+
+		loadingText.gameObject.SetActive(false);
+	}
+
+	// Hide the layer
+	public void hide()
+	{
+		contentRoot.SetActive(false);
+	}
+
+	// Start the game
+	private IEnumerator startGame()
+	{
+		float delay = 1.5f;
 		
+		hide();
+		PlayerStatsContainer.resetValues();
+		StageSystemManager.resetValues();
+		ScoreTargetController.resetValues();
+
+		loadingText.gameObject.SetActive(true);
+
+		
+
+		yield return new WaitForSeconds(delay);
+	
+		fadeImage.SetBool("isShowing", true);
+
+		yield return new WaitForSeconds(2.0f);
+
+		SceneManager.LoadSceneAsync("Scene_GameArea_Sanctuary");
 	}
 }
