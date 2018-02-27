b449805b6c65a1e7b76f4b5145713785a28c487b
Megawats777 <jedsemilla@gmail.com>
2018-02-27 15:59:12 -0500

    Created help screen classes and started work on the help screen layout

---
+++ Assets/Scripts/MainMenuScripts/MainMenu_HelpScreenLayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu_HelpScreenLayer : MonoBehaviour
+{
+
+	// External references
+	private MainMenu_LayerController mainMenu_LayerController;
+
+
+	// Called before start
+	void Awake()
+	{
+		mainMenu_LayerController = FindObjectOfType<MainMenu_LayerController>();
+	}
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+	// Show this layer
+	public void show()
+	{
+
+	}
+
+	// Hide this layer
+	public void hide()
+	{
+
+	}
+}
--- Assets/Scripts/MainMenuScripts/MainMenu_LayerController.cs
+++ Assets/Scripts/MainMenuScripts/MainMenu_LayerController.cs
@@ -9,12 +9,14 @@ public class MainMenu_LayerController : MonoBehaviour
 	// Layer references
 	[Header("Layer References"), SerializeField]
 	private MainMenu_WelcomeLayer welcomeScreen;
-
+	[SerializeField]
+	private MainMenu_HelpScreenLayer helpScreen;
 
     // Use this for initialization
     void Start()
     {
 		welcomeScreen.gameObject.SetActive(true);
+		helpScreen.gameObject.SetActive(false);
 		show_welcomeScreen();
     }
 
@@ -28,12 +30,27 @@ public class MainMenu_LayerController : MonoBehaviour
 
 	public void show_welcomeScreen()
 	{
+		welcomeScreen.gameObject.SetActive(true);
 		welcomeScreen.show();
 	}
 
 	public void hide_welcomeScreen()
 	{
 		welcomeScreen.hide();
+		welcomeScreen.gameObject.SetActive(false);
+	}
+
+
+	public void show_helpScreen()
+	{
+		helpScreen.gameObject.SetActive(true);
+		helpScreen.show();
+	}
+
+	public void hide_helpScreen()
+	{
+		helpScreen.hide();
+		helpScreen.gameObject.SetActive(false);
 	}
 
 	/*--Show and Hide layer methods end--*/
